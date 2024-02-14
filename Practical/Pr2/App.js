0ce017fdb0ac06d69c690daa53c5b81bcd18a094
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2024-02-14 10:32:42 +0530

    Create App.js

---
+++ Practical/Pr2/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+import TodoInput from './components/TodoInput';
+
+function App() {
+
+return (
+
+<div className="main-container">
+
+<div className="center-container">
+
+<TodoInput/>
+
+</div>
+
+</div>
+
+);
+
+}
+
+export default App;
