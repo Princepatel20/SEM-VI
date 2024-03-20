ecef94de2c6cd31e5fc151759e976617cbc747e4
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2024-03-20 18:42:42 +0530

    Create Pr4.js

---
+++ FSD/Pr4.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+res.writeHead(200, { "Content-Type": "text/plain" });
+res.end("This is the http server!!");
+});
+const PORT = 8085;
+server.listen(PORT, () => {
+console.log(`Server is running at http://localhost:${PORT}/`);
+});
