f19eb669eb8cb4a167350483eef4cf0c8728f5bf
Prince Gondaliya <100784026+Princepatel20@users.noreply.github.com>
2024-03-20 20:22:14 +0530

    Update Pr10.js

--- FSD/Pr10.js
+++ FSD/Pr10.js
@@ -2,22 +2,23 @@ Program:
 
 User.jsx
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
-name: String,
-email: String,
-age: Number,
+  name: String,
+  email: String,
+  age: Number,
 });
 module.exports = userSchema;
+
 db.jsx
 const mongoose = require("mongoose");
 mongoose
-.connect("mongodb://localhost:27017/?directConnection=true", {
-useNewUrlParser: true,
-useUnifiedTopology: true,
-})
-.then(() => console.log("Connected to MongoDB"))
-.catch((err) => console.error("Error connecting to MongoDB:", err));
+  .connect("mongodb://localhost:27017/?directConnection=true", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
 module.exports = mongoose;
 
 crud.jsx
@@ -26,29 +27,29 @@ const mongoose = require("./db.jsx");
 const userSchema = require("./models/user");
 const User = mongoose.model("User", userSchema);
 const main = async () => {
-try {
-const newUser = await User.create({
-name: "Vaidik Ghelani",
-email: "Vaid@example.com",
-age: 22,
-});
-console.log("New user created:", newUser);
-const allUsers = await User.find();
-console.log("All users:", allUsers);
-const updatedUser = await User.findOneAndUpdate(
-{ name: "Vaidik Ghelani" },
-{ age: 20 },
-{ new: true }
-);
-console.log("Updated user:", updatedUser);
-const deletedUser = await User.findOneAndDelete({ name: "Vaidik Ghelani" });
-console.log("Deleted user:", deletedUser);
-const remainingUsers = await User.find();
-console.log("Remaining users:", remainingUsers);
-} catch (error) {
-console.error("Error:", error);
-} finally {
-mongoose.disconnect();
+  try {
+    const newUser = await User.create({
+      name: "Prince Gondaliya",
+      email: "xyz@example.com",
+      age: 22,
+    });
+    console.log("New user created:", newUser);
+    const allUsers = await User.find();
+    console.log("All users:", allUsers);
+    const updatedUser = await User.findOneAndUpdate(
+      { name: "Vaidik Ghelani" },
+      { age: 20 },
+      { new: true }
+    );
+    console.log("Updated user:", updatedUser);
+    const deletedUser = await User.findOneAndDelete({ name: "Vaidik Ghelani" });
+    console.log("Deleted user:", deletedUser);
+    const remainingUsers = await User.find();
+    console.log("Remaining users:", remainingUsers);
+  } catch (error) {
+    console.error("Error:", error);
+  } finally {
+    mongoose.disconnect();
   }
 };
 main();
